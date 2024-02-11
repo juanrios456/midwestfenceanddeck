083643ca1337d1d32eae7e6eb8b07996d9245b11
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2024-02-11 13:05:38 -0600

    Updated the metadata for the index page
    Added a title tag and a meta description with SEO research behind it. Added Open Graph tags (title, description, image, type, and URL). A sitemap was included with the priority of the index page being 1.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 import { Header } from "./header";
 import { Services } from "./services";
 import { SlideShows } from "./slideshows";
+import { Metadata } from 'next'
+
+export const metadata:Metadata = {
+  title: 'Fence Contractor | Deck Builder Near Kansas - Midwest Fence and Deck',
+  description: `Quality fence installations and Deck construction/installation at an affordable price. 
+  Over 30 years of experience with wood fences, chain link fences, vinyl fences, aluminum fences, steel fences, stairs, steps, roofs, railings, renovations, repairs, and special ordered designs.`,
+  openGraph: {
+    title: 'Midwest Fence and Deck | Fence Contractor | Deck Builder',
+    description: 'Located in Kansas, with over 30 years of experience building and designing decks/fences. Contact Us today for a free estimate.',
+    images: ['https://midwestfenceanddeck.com/deck-1.jpg'],
+    url: 'https://midwestfenceanddeck.com',
+    type: 'website',
+  }
+} 
 
 export default function Home() {
   return (
